de355b6a01dec48765ed6830e97022df966b29c6
Meagan Jungman <meagan.jungman@gmail.com>
2016-01-25 12:41:26 -0600

    done

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -2,5 +2,10 @@
 # returns a properly formatted phone number.
 
 def format_phone_number(phone_number_str)
-  
+phone_number_str.gsub!(/\D/, "")
+area_code = phone_number_str[0..2]
+phone_beginning = phone_number_str[3..5]
+phone_end = phone_number_str[6..9]
+
+return "(#{area_code}) #{phone_beginning}-#{phone_end}"
 end
